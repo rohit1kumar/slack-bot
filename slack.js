4d8be70dbca99768a7c73a0057285aeaf69a3d7c
Rohit Kumar <kumar1rohit@outlook.com>
2023-07-17 00:13:07 +0530

    respond to member_joined_channel, app_mention event

--- slack.js
+++ slack.js
@@ -1,32 +1,52 @@
-const { App } = require('@slack/bolt')
 require('dotenv').config()
-const executeQuery = require('./query.js')
+const { App } = require('@slack/bolt')
+const executeQuery = require('./query')
+
+const BOT_ID = 'U05H8754KR9' // databot's user id, to prevent from responding to itself when joining a channel
 
 const app = new App({
 	signingSecret: process.env.SLACK_SIGNING_SECRET,
 	token: process.env.SLACK_BOT_TOKEN,
-	appToken: process.env.SLACK_APP_TOKEN,
-	socketMode: true
+	// appToken: process.env.SLACK_APP_TOKEN,
+	logLevel: 'debug'
 })
 
-app.command('/ask', async ({ command, ack, say }) => {
+app.event('app_mention', async ({ event, client }) => {
+	const { channel, text } = event
 	try {
-		await ack()
-		const resp = await executeQuery(`${command.text}`)
-		console.log('resp from slack', command.text)
-		console.log('resp from bot', resp)
-		await say(resp || 'Sorry, I did not understand that. Please try again.')
+		const userText = text.replace(/^<[^>]+>\s*/, '')
+		const resp = await executeQuery(userText)
+		await client.chat.postMessage({
+			channel,
+			text: resp || 'Sorry, I did not understand that. Please try again.'
+		})
 	} catch (error) {
 		console.error(error)
-		await say(
-			'Sorry, I did not understand that. Please try again, or contact the developer.'
-		)
+	}
+})
+
+app.event('member_joined_channel', async ({ event, client }) => {
+	let text
+	const { user, channel } = event
+	if (user === BOT_ID) {
+		text = "I'm Databot and I can help you with your data needs"
+	} else {
+		text = `Welcome to the channel, <@${user}>, I'm Databot and I can help you with your data needs.`
+	}
+
+	try {
+		await client.chat.postMessage({ channel, text })
+	} catch (error) {
+		console.error(error)
 	}
 })
 
 ;(async () => {
-	// Start the app
-	await app.start(process.env.PORT || 3000)
-
-	console.log('⚡️ Bolt app is running!')
+	try {
+		// Start the app
+		await app.start(process.env.PORT || 3000)
+		console.log('⚡️ Bolt app is running!')
+	} catch (error) {
+		console.error('Error starting Bolt app:', error)
+	}
 })()
